f7ca9c622222f2e5ffde051a09766e54c9f850a5
kadetXx <techkadet@gmail.com>
2023-02-06 23:34:53 +0100

    chore: cleanup comments

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -15,19 +15,6 @@ export const useAuth = () => {
     window.location.replace(`${mainApp}?callbackUrl=${callbackUrl}`);
   }, [isLoading, isAuthenticated]);
 
-  useEffect(() => {
-    console.log(user, ">>");
-  }, [user]);
-
-  /**
-   * email: "techkadet@gmail.com"
-     email_verified: false
-     name: "techkadet@gmail.com"
-     nickname: "techkadet"
-     picture: "https://s.gravatar.com/avatar/d0c3a56efd206edc5c3d004def2efefc?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fte.png"
-     updated_at: "2023-02-06T02:23:35.846Z"
-   */
-
   return {
     user,
     logout,
