bba3b60f6f77ea457be7753bc47ba4c096b5c5b8
kadetXx <techkadet@gmail.com>
2023-02-10 16:24:16 +0100

    fix: fixed auth0 logout

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Auth0Provider
+      useRefreshTokens
+      cacheLocation="localstorage"
       domain={import.meta.env.VITE_AUTH0_ISSUER_BASEURL}
       clientId={import.meta.env.VITE_AUTH0_CLIENTID}
       authorizationParams={{
